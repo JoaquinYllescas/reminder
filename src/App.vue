98629b35deb6fe4baed4b49f9fc915faf332ebd3
JoaquinYllescas <jyllescasruiz@gmail.com>
2017-07-20 17:41:44 +0100

    Home Page in progress

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
   <div id="app">
 
     <header>
-       <ul>
-        <li><router-link to="/home">Home</router-link></li>
-        <li><router-link to="/listado">Listado</router-link></li>
-        <!-- <li><router-link to="/detalle">Detalle</router-link></li> -->
-        <li><router-link to="/crear">Crear</router-link></li>
-      </ul>
+      <h1>Birthday Reminder</h1>
+      <nav>
+        <div v-on:click="menu();" class="hamburguer"><i class="fa" :class="menuClass" aria-hidden="true"></i></div>
+         <ul class="animated" v-bind:class="{  open: isOpen, slideInLeft: isOpen, slideOutLeft: !isOpen }"  >
+          <li><router-link to="/home">Birthday List</router-link></li>
+          <li><router-link to="/listado">Add Birthday</router-link></li>
+          <li><router-link to="/detalle">My Account</router-link></li>
+          <li><router-link to="/crear">Edit Birthday</router-link></li>
+        </ul>
+      </nav>
     </header>
+    
+    
+   
 
     <router-view></router-view>
 
@@ -21,7 +28,40 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  mounted(){
+    
+  },
+  data() {
+    return{
+      isActive: true,
+      menuClass: 'fa_bars',
+      isOpen: false,
+      menuSlide: ''
+    }
+  },
+
+  methods: {
+
+    /* Funcionalidad de abrir y cerrar menu */
+    menu: function(){
+      //console.log( this.isActive);
+      if(this.isActive == true){
+        this.isActive = false;
+        this.menuClass = 'fa_times'
+        this.menuSlide = 'slideInLeft'
+
+        this.isOpen = true;
+      }else{
+        this.isActive = true;
+        this.menuClass = 'fa_bars'
+
+        this.isOpen = false;
+        this.menuSlide = 'slideOutLeft'
+      }
+      
+    }
+  }
 }
 </script>
 
@@ -42,7 +82,11 @@ html, body{
   height:100%;
   box-sizing:border-box;
 }
+img{
+  max-width:100%;
+}
 #app{
+  width:100%;
   height:100%;
   font-family: 'Josefin Sans', sans-serif;
   -moz-osx-font-smoothing: grayscale;
@@ -55,12 +99,72 @@ html, body{
   background-image:url("./assets/bg.jpg");
   background-size:cover;
   background-position:center;
+  position:fixed;
+  top:0px;
+  left:0px;
 
   header{
     display:block;
     width:100%;
     height:50px;
     background-color:#004a7a;
+
+    nav{
+      position:absolute;
+      width:100%;
+
+      .hamburguer{
+        color:#ffffff;
+        font-size:30px;
+        position: absolute;
+        top: 10px;
+        left: 15px;
+        cursor:pointer;
+      }
+
+      ul{
+        position: fixed;
+        width:80%;
+        height:100%;
+        background-color:#002c49;
+        padding-top:40px;
+        margin:0px;
+        padding:0px;
+        margin-top:50px;
+        padding-top:40px;
+
+        li{
+          text-align:left;
+          list-style:none;
+          font-size:30px;
+          font-weight:300;
+          height:50px;
+          line-height:50px;
+          padding-left:40px;
+
+          &:hover{
+            background-color:#33566d;
+          }
+
+          a{
+            display:block;
+            text-decoration:none;
+            color:#ffffff;
+          }
+        }
+       
+      }
+    }
+    h1{
+      margin:0px;
+      padding:0px;
+      font-size:20px;
+      font-weight:300;
+      color:#ffffff;
+      float:right;
+      line-height:50px;
+      margin-right:20px;
+    }
   }
 
   footer{
@@ -70,10 +174,20 @@ html, body{
     background-color:#004a7a;
     position:absolute;
     bottom:0px;
+
+    h1{
+      margin:0px;
+      padding:0px;
+      font-size:14px;
+      color:#8da7c2;
+      line-height:56px;
+    }
   }
 
-  h1{
-    margin:0px;
-  }
+
+
+  
+
+
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="home">
+     <div class="logo" v-bind:class="{  small: loaded}">
+      <h1>Birthday Reminder</h1>
+      <img src="../assets/logo.png" alt="">
+    </div>
+
+
+    <div v-if="loaded==true" class="button">
+      <a class="login" href="">
+        Login
+      </a>
+      <a class="createAccount" href="">
+        Create Account
+      </a>
+    </div>
+
+    <div v-else class="logo">
+      <div class="loading">
+        Loading...
+      </div>
+    </div>
+
+    
+    
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      loaded: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.home{
+  .logo{
+    width:60%;
+    margin:0px auto;
+
+    
+
+    
+    
+   
+
+    h1{
+      color:#ffffff;
+      font-weight:100;
+      font-style:normal;
+      margin-top:30%;
+    }
+
+    &.small{
+      transition-property: width;
+      transition-duration: 0.3s;
+      transition-timing-function: linear;
+      transition-delay: 0.5s;
+      width:40%;
+    }
+
+  }
+
+  .loading{
+    font-size:18px;
+    color:#ffffff;
+    padding-top:50px;
+    font-weight:100;
+  }
+  
+  .login, .createAccount{
+    display:block;
+    max-width:50%;
+    margin:20px auto 0px auto;
+    padding:20px 50px;
+    background-color:#b3cfe1;
+    text-decoration:none;
+    color:#000000;
+    box-shadow:0px 3px 5px rgba(0,0,0,.8);
+  }
+  
+
+}
+
+</style>
